bf21b176dfadc6b71ab84880c654763f0b1d3a6f
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-17 18:28:07 -0300

    :sparkles: feat: adicionado metodo findByNomeDoUsuario

--- src/main/java/com/factory/contabancaria/repository/ContasRepository.java
+++ src/main/java/com/factory/contabancaria/repository/ContasRepository.java
@@ -2,8 +2,13 @@ package com.factory.contabancaria.repository;
 
 import com.factory.contabancaria.model.ContasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ContasRepository extends JpaRepository<ContasModel, Long> {
+    List<ContasModel> findByNomeDoUsuario( String nomeDoUsuario);
 }
