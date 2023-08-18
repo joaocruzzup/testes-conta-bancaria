79447b28cdd51d9c9a00ad47cb75876a2835b4c0
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-08-17 21:53:24 -0300

    :sparkles: feat: criado exceções

---
+++ src/main/java/com/factory/contabancaria/exception/ContaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.factory.contabancaria.exception;
+
+public class ContaNaoEncontradaException extends RuntimeException{
+    public ContaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/factory/contabancaria/exception/ParametrosNaoPreenchidosException.java
@@ -0,0 +1,7 @@
+package com.factory.contabancaria.exception;
+
+public class ParametrosNaoPreenchidosException extends RuntimeException{
+    public ParametrosNaoPreenchidosException(String message) {
+        super(message);
+    }
+}
